30cbff459b6acb643322de687c347afa1e89ad13
Oleg Saienko <Oleg@example.com>
2020-08-14 08:14:04 +0300

    initial commit

---
+++ src/main/java/com/olsa/DndCharGenerator/DndCharGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.olsa.DndCharGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndCharGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndCharGeneratorApplication.class, args);
+	}
+
+}
